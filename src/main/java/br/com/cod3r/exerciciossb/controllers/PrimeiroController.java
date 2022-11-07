62304b2a8ab884e7c6a550b8b678bf2b1d633333
Zelialdo Sueldo da Silva Lima <71799490+zelialdo@users.noreply.github.com>
2022-11-07 07:12:17 -0300

    a

--- src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/PrimeiroController.java
@@ -1,7 +1,6 @@
 package br.com.cod3r.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,9 +12,9 @@ public class PrimeiroController {
 		return "Olá Spring Boot";
 	}
 	
-	@PostMapping(path = {"/saudacao"})
-	public String saudacao() {
-		return "Olá Spring Boot(POST)";
-	}
+//	@PostMapping(path = {"/saudacao"})// simulando erros... dois metodos na mesma URL 
+//	public String saudacao() {
+//		return "Olá Spring Boot(POST)";
+//	}
 }
  
---
+++ src/main/java/br/com/cod3r/exerciciossb/models/Cliente.java
@@ -0,0 +1,18 @@
+package br.com.cod3r.exerciciossb.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Cliente {
+	
+	private int id;
+	private String nome;
+	private String cpf;
+	
+	
+
+}
