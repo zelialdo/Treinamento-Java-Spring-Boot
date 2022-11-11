dd54cce0e335ac52e60cae82c3c6de5b6bf84fdb
Zelialdo Sueldo da Silva Lima <71799490+zelialdo@users.noreply.github.com>
2022-11-10 23:54:21 -0300

    Update ClienteController.java

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
@@ -3,6 +3,7 @@ package br.com.cod3r.exerciciossb.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.cod3r.exerciciossb.models.Cliente;
@@ -20,5 +21,10 @@ public class ClienteController {
 	@GetMapping("/{id}") 
 	public Cliente obterClientePorId1(@PathVariable int id) {
 		return new Cliente(id, "Maria", "987.654.321-00");
+	}
+	
+	@GetMapping
+	public Cliente obterClientePorId2(@RequestParam(name = "id", defaultValue = "1") int id) {
+		return new Cliente(id, "João Augusto", "111.222.333-00");
 	}
 }
