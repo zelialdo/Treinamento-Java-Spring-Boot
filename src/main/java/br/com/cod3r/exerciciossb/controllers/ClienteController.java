32194413a0d3047d0edc12ece8e1b160b3cd9b29
Zelialdo Sueldo da Silva Lima <71799490+zelialdo@users.noreply.github.com>
2022-11-10 23:29:31 -0300

    Parametros web service #2
    passando parametros para web serrvice #02

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.cod3r.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +15,10 @@ public class ClienteController {
 	public Cliente obterCliente() {
 		return new Cliente(28, "Pedro", "123.456.789-00");
 	}
-
+	
+	// primeira forma de passagem de paramentros ( através da URL )
+	@GetMapping("/{id}") 
+	public Cliente obterClientePorId1(@PathVariable int id) {
+		return new Cliente(id, "Maria", "987.654.321-00");
+	}
 }
